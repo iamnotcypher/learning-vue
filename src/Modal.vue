0cb753e32370b37f3980f0b0945eb90d2664b5d3
unknown <huzitaiwo@gmail.com>
2023-03-08 19:26:06 +0100

    created Modal component

---
+++ src/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, in.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: #fff;
+    border-radius: 5px;
+  }
+  .backdrop {
+    position: fixed;
+    top: 0;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+  }
+</style>
